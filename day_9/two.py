51cb23a83aa7a457936a5674d61a550f2051907d
Synedh <contact@synedh.fr>
2020-12-09 12:11:33 +0100

    Day 9: Cleaned, removed useless var renamed some vars.

--- day_9/two.py
+++ day_9/two.py
@@ -1,22 +1,20 @@
-def get_incorrect_num(values):
+def get_target(values):
     lasts = values[:25]
     for value in values[25:]:
         if not any((value - i) in lasts for i in lasts):
             return value
         lasts = lasts[1:] + [value]
-    return 0
+    return -1
 
-values = [int(v) for v in open('input').readlines()]
-incorrect = get_incorrect_num(values)
-print(incorrect)
+values = [int(v) for v in open('input')]
+target = get_target(values)
+print(target)
 
 for i, value in enumerate(values):
-    test = value
     items = [value]
-    while test < incorrect:
+    while sum(items) < target:
         i += 1
-        test += values[i]
         items.append(values[i])
-    if test == incorrect:
+    if sum(items) == target:
         print(max(items) + min(items))
         exit()
