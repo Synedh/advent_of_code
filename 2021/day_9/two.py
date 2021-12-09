d84df7c0250a31752b64345e58595c00fb31b734
Synedh <contact@synedh.fr>
2021-12-09 18:19:45 +0100

    Update day9/two

--- 2021/day_9/two.py
+++ 2021/day_9/two.py
@@ -1,19 +1,14 @@
-def get_basin(matrice, i, j):
-    if 0 <= i < len(matrice) and 0 <= j < len(matrice[i]) and matrice[i][j] != '9':
-        matrice[i][j] = '9'
-        return (
-            1
-            + get_basin(matrice, i - 1, j)
-            + get_basin(matrice, i + 1, j)
-            + get_basin(matrice, i, j - 1)
-            + get_basin(matrice, i, j + 1)
-        )
+matrice = [*map(list, open('input').read().split('\n'))]
+
+def get_basin(x, y):
+    if x in range(len(matrice)) and y in range(len(matrice[x])) and matrice[x][y] != '9':
+        matrice[x][y] = '9'
+        return 1 + sum(get_basin(x + dx, y + dy) for dx, dy in [(-1, 0), (1, 0), (0, -1), (0, 1)])
     return 0
 
-matrice = [*map(list, open('input').read().split('\n'))]
 basins = []
 for i in range(len(matrice)):
     for j in range(len(matrice[i])):
-        basins.append(get_basin(matrice, i, j))
+        basins.append(get_basin(i, j))
 basins.sort(reverse=True)
 print(basins[0] * basins[1] * basins[2])
