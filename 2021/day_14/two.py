d23857f1e3d3d773606de654efc5994f84d1f752
Synedh <contact@synedh.fr>
2021-12-14 13:06:58 +0100

    Day 14 renamed variable.

--- 2021/day_14/two.py
+++ 2021/day_14/two.py
@@ -1,12 +1,12 @@
 template, rules = open('input').read().split('\n\n')
 rules = dict(rule.split(' -> ') for rule in rules.splitlines())
 pairs = { pair: template.count(pair) for pair in rules }
-count = { char: template.count(char) for char in rules.values() }
+chars = { char: template.count(char) for char in rules.values() }
 
 for i in range(40):
     for pair, value in pairs.copy().items():
         pairs[pair] -= value
         pairs[pair[0] + rules[pair]] += value
         pairs[rules[pair] + pair[1]] += value
-        count[rules[pair]] +=  value
-print(max(count.values()) - min(count.values()))
+        chars[rules[pair]] +=  value
+print(max(chars.values()) - min(chars.values()))
