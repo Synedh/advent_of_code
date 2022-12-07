4e661d9b9854f443540e702293cede8039306150
Synedh <contact@synedh.fr>
2022-12-07 08:49:49 +0100

    Started 2022/day_7

---
+++ 2022/day_7/one.py
@@ -0,0 +1,31 @@
+import json
+from itertools import chain
+
+lines = open('input').read().strip().splitlines()
+
+def exec_line(i: int=0):
+    children = {}
+    while i < len(lines):
+        line = lines[i]
+        i += 1
+        if line.startswith('$ cd'):
+            if line.split()[-1] == '..':
+                break
+            children[line.split()[-1]], i = exec_line(i)
+        elif line.startswith('$ ls'):
+            continue
+        else:
+            value, name = line.split()
+            if value != 'dir':
+                children[name] = { 'size': int(value) }
+    size = sum(child['size'] for child in children.values())
+    return {'children': children, 'size': size}, i
+
+def sizes(tree):
+    child_sizes = [sizes(child) for child in tree['children'].values() if child.get('children')]
+    return [tree['size']] + list(chain(*[ele if isinstance(ele, list) else [ele] for ele in child_sizes]))
+
+tree, _ = exec_line(1)
+print(json.dumps(tree, indent=4))
+
+print(sum(size for size in sizes(tree) if size <= 100000))
