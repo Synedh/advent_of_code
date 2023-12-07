ce2daa2e313f0ed034fa738aa4d454e723b61503
Synedh <contact@synedh.fr>
2023-12-07 10:01:54 +0100

    2023 day 7

---
+++ 2023/day_7/two.py
@@ -0,0 +1,20 @@
+from collections import Counter
+
+card_values = ['J', '2', '3', '4', '5', '6', '7', '8', '9', 'T', 'Q', 'K', 'A']
+
+def order_hand(hand: str):
+    try:
+        most_common = Counter(hand.replace('J', '')).most_common(1)[0][0]
+    except IndexError:
+        most_common = 'J'
+    faked_hand = hand.replace('J', most_common)
+    hand_count = sorted(list(Counter(faked_hand).values()), reverse=True)
+    hand_type = f'{"".join(map(str, hand_count)):<5}'
+    hand_cards = ''.join(f'{card_values.index(card):>2}' for card in hand)
+    return hand_type + hand_cards
+
+sorted_hands = sorted(
+    open('input').read().splitlines(),
+    key=lambda line: order_hand(line.split()[0])
+)   
+print(sum(int(hand.split()[1]) * (i + 1) for i, hand in enumerate(sorted_hands)))
