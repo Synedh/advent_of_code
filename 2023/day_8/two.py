dd2e18ace055eca655d6cab3bf8c19b44ff4eefe
Synedh <contact@synedh.fr>
2023-12-09 09:50:34 +0100

    2023 day 8

--- 2023/day_8/two.py
+++ 2023/day_8/two.py
@@ -2,15 +2,14 @@ from itertools import cycle
 from math import lcm
 
 directions, _, *str_ways = open('input').read().splitlines()
-ways = {way[0:3]: (way[7:10], way[12:15]) for way in str_ways}
+ways = {way[0:3]: {'L': way[7:10], 'R': way[12:15]} for way in str_ways}
 
 positions = [way for way in ways if way.endswith('A')]
 totals = [0] * len(positions)
 for i, pos in enumerate(positions):
-    c = cycle(int(d == 'R') for d in directions)
+    c = cycle(directions)
     while not pos.endswith('Z'):
         totals[i] += 1
         pos = ways[pos][next(c)]
 
-print(totals[0])
 print(lcm(*totals))
