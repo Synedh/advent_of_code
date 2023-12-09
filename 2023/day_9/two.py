67f5f5f9ab8cc7af99766c353ac4e5123825ff3e
Synedh <contact@synedh.fr>
2023-12-09 09:50:46 +0100

    2023 day 9

---
+++ 2023/day_9/two.py
@@ -0,0 +1,7 @@
+report = [[*map(int, line.split())] for line in open('input')]
+
+def rec_derivative(seq):
+    dv = [y - x for x, y in zip(seq, seq[1:])]
+    return seq[-1] + (rec_derivative(dv) if any(dv) else 0)
+
+print(sum(rec_derivative(line[::-1]) for line in report))
