d18a004b123baf7f4452d8c281d1c83b4734c1a8
Synedh <contact@synedh.fr>
2024-04-13 09:02:52 +0200

    2024/day_13

---
+++ 2024/day_13/two.py
@@ -0,0 +1,16 @@
+import re
+
+systems = [map(int, re.findall(r'\d+', system)) for system in open('input').read().split('\n\n')]
+
+def system_solve(a1, b1, c1, a2, b2, c2):
+    y = (c2 * a1 - c1 * a2) / (a1 * b2 - a2 * b1)
+    x = (c1 - b1 * y) / a1
+    return x, y
+
+total = 0
+for a1, a2, b1, b2, c1, c2 in systems:
+    x, y = system_solve(a1, b1, c1 + 10000000000000, a2, b2, c2 + 10000000000000)
+    if int(x) == x and int(y) == y:
+        total += 3 * x + y
+
+print(int(total))
