e1b835b4bb07203a1acff00f4e52d5b969335be7
Synedh <contact@synedh.fr>
2024-12-07 10:28:58 +0100

    2024/day_7

---
+++ 2024/day_7/two.py
@@ -0,0 +1,21 @@
+import re
+
+def resolve(result, members):
+    try:
+        mem1, mem2, *mems = members
+    except ValueError:
+        return members[0] == result
+    if mem1 > result:
+        return False
+    return (
+        resolve(result, [mem1 + mem2, *mems])
+        or resolve(result, [mem1 * mem2, *mems])
+        or resolve(result, [int(f'{mem1}{mem2}'), *mems])
+    )
+
+total = 0
+for line in open('input'):
+    result, *members = map(int, re.findall(r'\d+', line))
+    if resolve(result, members):
+        total += result
+print(total)
