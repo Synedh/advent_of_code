4dea93a0489aca585c4ed7fa8480eaad6849578a
Synedh <contact@synedh.fr>
2024-12-10 08:50:21 +0100

    2024/day_10

---
+++ 2024/day_10/one.py
@@ -0,0 +1,21 @@
+topo = [list(map(int, l)) for l in open('input').read().splitlines()]
+
+trailheads = [(x, y) for x, line in enumerate(topo) for y, val in enumerate(line) if val == 0]
+
+def get_neightbours(x, y):
+    neightbours = []
+    for dx, dy in (-1, 0), (0, -1), (1, 0), (0, 1):
+        if -1 < x + dx < len(topo) and -1 < y + dy < len(topo[0]):
+            neightbours.append((x + dx, y + dy))
+    return neightbours
+
+def walk_path(x, y, height):
+    if topo[x][y] == 9:
+        return {(x, y)}
+    paths = set()
+    for x, y in get_neightbours(x, y):
+        if topo[x][y] == height + 1:
+            paths |= walk_path(x, y, height + 1)
+    return paths
+
+print(sum(len(walk_path(*trailhead, 0)) for trailhead in trailheads))
